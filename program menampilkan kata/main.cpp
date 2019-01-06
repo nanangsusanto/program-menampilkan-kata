96dabf693f23a3cfd41091485e348e331f00859a
nanangsusanto <46389404+nanangsusanto@users.noreply.github.com>
2019-01-06 00:02:39 -0800

    Add files via upload

---
+++ program menampilkan kata/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int kalimat ()
+{
+    int x;
+    cout<< "berapa kalimat yang ingin anda cetak : "; cin>>x;
+    for (int a=1;a<=x;a++)
+    {
+        cout << "\n algoritma dan pemrograman";
+
+    }
+}
+    int main()
+    {
+        kalimat ();
+        getch ();
+        return 0;
+    }
+
